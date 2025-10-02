45f701d74843b9d35b2bacb46746f58766993d7d
Marvellous Ogunleke <marvellousabiola08@gmail.com>
2025-10-02 03:28:55 +0100

    save

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,45 @@
-import React from 'react'
+"use client"
+import React,{useState} from 'react'
 import Link from 'next/link'
+import { MenuSquare, X } from 'lucide-react'
+
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const handleToggle =()=>{
+    setIsOpen(true)
+  }
+  const handleClose =()=>{
+    setIsOpen(false)
+  }
   return (
+    <>
     <div className='flex w-full items-center justify-between px-4 sm:px-6 md:px-6 lg:px-17 border-b border-gray-500 py-4 bg-green-50'>
       <div className='text-xl font-bold text-green-800'>Nigeria</div>
-      <nav className='flex space-x-4 text-green-900 gap-7 pr-20'>
+      <div className='flex'>
+      <nav className='hidden md:flex space-x-4 text-green-900 gap-7 pr-20'>
         <Link href="/" className='hover:text-green-500'>Home</Link>
         <Link href='/History' className='hover:text-green-500'>History</Link>
         <Link href='#' className='hover:text-green-500'>Support</Link>
         <Link href='#' className='hover:text-green-500'>FAQs</Link>
-
-        
       </nav>
+      {!isOpen? <MenuSquare onClick={handleToggle} className='text-green-800 text-2xl'/>
+       :<X onClick={handleClose} className='text-green-800 text-2xl'/>}
+      
+      
+</div> 
     </div>
+    {isOpen &&
+        <div className='m-8 z-20'>
+          <nav className='flex flex-col md:hidden space-y-2 text-green-900 gap-7 '>
+        <Link href="/" className='hover:text-green-500'>Home</Link>
+        <Link href='/History' className='hover:text-green-500'>History</Link>
+        <Link href='#' className='hover:text-green-500'>Support</Link>
+        <Link href='#' className='hover:text-green-500'>FAQs</Link>
+      </nav>
+        </div>
+}
+</>
   )
 }
 
